6abab0030c4ce1a7fc3a85d9a59589994e9ddda8
Fr3m3l1 <fr3m3l@gmail.com>
2024-12-04 18:00:39 +0100

    fixed issue

--- nutritrack/main.py
+++ nutritrack/main.py
@@ -5,7 +5,7 @@ import time
 import sys
 import os
 from dotenv import load_dotenv
-from nutritrack.processor.cron_job import cron_job
+from processor.cron_job import cron_job
 
 from data import create_nutrition_table
 
